60e1212fb3a254e68f009e73945b423d982204d3
kphatompong <53294431+kphatompong@users.noreply.github.com>
2019-07-26 09:48:19 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,8 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'IG+YXYoYXHnnyNt5Ugfp7c7ME85Cndf63UaJkZICfoa6bDZpEooYURSFFCU7PV+ALGK1yAUxiW+vFrHZJFNIyEmHsWLrx60Y6XMf4R7pr6+mfhGdbxO8xlpxGjPz0uGKb+J4akxmJQCIL0GikF5i6AdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = '9N3Z9mA12ipd5FbU9tbfb5bbUYPjgXpGkaASHRNZ8VIPA4MF79MI4JH5HNDRK/bSwZdD7VroI2S1Nqs7m6PgZDznst/AQr46fWFCCSvH36ic35TeFQ4qbfaqOEAhaGBqqDnOwT9pRsAql4VsSsmklQdB04t89/1O/w1cDnyilFU=
+'; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
